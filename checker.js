43b4b1745edf4bc14c30300ae0c7316ff942f873
takiguru <takiguru.wow@gmail.com>
2019-05-17 17:14:26 +0200

    working solution for testing

--- checker.js
+++ checker.js
@@ -1,15 +1,50 @@
 //Configuration
 const retailURL =
-  "https://smp-device-content.apple.com/static/region/v2/config.json";
+  "https://cors-anywhere.herokuapp.com/https://smp-device-content.apple.com/static/region/v2/config.json";
 const betaURL =
-  "https://smp-device-content.apple.com/static/region/v2/config-beta.json";
+  "https://cors-anywhere.herokuapp.com/https://smp-device-content.apple.com/static/region/v2/config-beta.json";
 
-async function fetchJson(url) {
-  return await fetch(url);
+async function fetchAppleConfiguration(url) {
+  try {
+    const resp = await fetch(url, {
+      headers: {
+        Origin: "https://smp-device-content.apple.com"
+      }
+    });
+    return await resp.json();
+  } catch (err) {
+    throw new Error("Configuration fetch failed:" + err.toString());
+  }
+}
+
+async function fetchCountryCode() {
+  try {
+    let resp = await fetch("https://ipinfo.io/json");
+    resp = await resp.json();
+    return resp.country;
+  } catch (err) {
+    throw new Error("Ip fetch failed" + err.toString());
+  }
 }
 
 (async () => {
-  const retailConfiguration = await fetchJson(retailURL);
-  const betaConfiguration = await fetchJson(betaURL);
-  console.log(retailConfiguration, betaConfiguration);
+  const retailConfiguration = await fetchAppleConfiguration(retailURL);
+  const betaConfiguration = await fetchAppleConfiguration(betaURL);
+  const countryCode = await fetchCountryCode();
+  const retailElement = document.getElementById("enabledRetail");
+  const betaElement = document.getElementById("enabledBeta");
+  const regionElement = document.getElementById("region");
+  const dataContainer = document.getElementById("data-container");
+  const loadingContainer = document.getElementById("loading-container");
+  regionElement.innerHTML = `Your country code: ${countryCode}`;
+  if (retailConfiguration.SupportedRegions[countryCode]) {
+    retailElement.style.color = "green";
+    retailElement.innerHTML = "YES :)";
+  }
+  if (betaConfiguration.SupportedRegions[countryCode]) {
+    betaElement.style.color = "green";
+    betaElement.innerHTML = "YES :)";
+  }
+  loadingContainer.style.display = "none";
+  dataContainer.style.display = "block";
 })();
